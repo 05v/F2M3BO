0c0b3aae858bf05831d8ebcd06cbb21d9e935114
vladverheij <vladverheij@gmail.com>
2022-04-05 12:55:37 +0200

    Added Lichtsterkte

--- dashboard/script/script.js
+++ dashboard/script/script.js
@@ -1,1 +1,32 @@
-console.log("dababy")
+"use strict"
+const timeDelay = 10000; // time delay refresh data
+let refreshTimer = window.setInterval(renderData, timeDelay); // timer data opvragen van server
+
+// jouw persoonlijke URL
+const mijnDataURL = "https://data.softwaredeveloper.amsterdam/api/device/04799dfc/latest";
+
+const dataDiv = document.getElementById("dataDiv"); // hier komt de data
+
+async function getSensorData() {
+    let url = mijnDataURL;
+    try {
+        let response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function renderData() {
+    let measurementRaw = await getSensorData();
+    let measurement = measurementRaw.data[0];
+    console.log(measurement);
+
+    // Nieuwe Javascript date maken met de de datum en tijd van de meting
+    let datum = new Date(measurement.datum);
+
+    // De meting gegevens wegschrijven naar de div                
+    dataDiv.innerText = `${measurement.value3}`;
+}
+
+renderData(); // start immediately
